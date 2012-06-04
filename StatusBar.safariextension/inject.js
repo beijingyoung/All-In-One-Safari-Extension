38209a8d332efc47c15a5a90c9d1f3dc93ab579f
Beijing Young Studios <twitter@beijingyoung.com>
2012-06-04 16:52:14 +0800

    Revert "Revert "修复状态吧不隐藏 重建 删除 style.css""
    This reverts commit fd2cef38b2e49f500492f89acb55eb4f34e43049.

--- StatusBar.safariextension/inject.js
+++ StatusBar.safariextension/inject.js
@@ -1,8 +1,8 @@
 var statusBar;
 
-if(window.top === window) {
-  
-  if(document.readyState === 'complete')
+if (window.top === window) {
+
+  if (document.readyState === 'complete')
     ready();
   else
     document.addEventListener('DOMContentLoaded', ready);
@@ -14,9 +14,7 @@ if(window.top === window) {
     }
   }, false);
 
-  function ready() {
-    document.body.addEventListener('mouseover', hover);
-  }
+  function ready() { document.body.addEventListener('mouseover', hover); }
 
   function hover(e) {
     var el = e.target;
@@ -36,23 +34,27 @@ if(window.top === window) {
   }
 
   function displayStatus(text) {
-    if(!statusBar) {
 
-      statusBar = document.createElement('div');
+    if (!statusBar) {
+      statusBar = document.createElement('p');
       statusBar.id = 'BeijingYoungStudiosStatusBar';
-
-      statusBar.setAttribute("style", "position: fixed; left: 0; bottom: 0; z-index: 999999999999999; overflow: auto; white-space: nowrap; width: auto; margin: 0; padding: .15em; background: -webkit-linear-gradient(top, #FFF 0px, #B0B0B0 1px, #A7A7A7); color: #333; text-shadow: 0 1px 0 #D0D0D0; border: 1px solid #6A6A6A; border-left: none; border-top-right-radius: 3px; font: 12px \"Lucida Grande\";");
-
+      statusBar.setAttribute("style", 'position: fixed; left: 0; bottom: 0; z-index: 999999999999999; overflow: hidden; white-space: nowrap; width: auto; margin: 0; padding: .15em; background: -webkit-linear-gradient(top, #FFF 0px, #B0B0B0 1px, #A7A7A7); color: #333; text-shadow: 0 1px 0 #D0D0D0; border: 1px solid #6A6A6A; border-left: none; border-top-right-radius: 3px; font: 12px "Lucida Grande"; opacity: 0; -webkit-transition: opacity 100ms ease-out;');
       document.body.appendChild(statusBar);
-
     }
+
     statusBar.innerText = text;
-    setTimeout(function() { statusBar.className = 'active'; }, 1);
+
+    setTimeout(function() {
+      statusBar.className = 'active';
+      statusBar.setAttribute("style", 'position: fixed; left: 0; bottom: 0; z-index: 999999999999999; overflow: hidden; white-space: nowrap; width: auto; margin: 0; padding: .15em; background: -webkit-linear-gradient(top, #FFF 0px, #B0B0B0 1px, #A7A7A7); color: #333; text-shadow: 0 1px 0 #D0D0D0; border: 1px solid #6A6A6A; border-left: none; border-top-right-radius: 3px; font: 12px "Lucida Grande"; opacity: 1; -webkit-transition: opacity 100ms ease-out;');
+    }, 1);
+
   }
 
   function hideStatus() {
-    if(statusBar)
+    if (statusBar)
       statusBar.className = '';
+      statusBar.setAttribute("style", 'position: fixed; left: 0; bottom: 0; z-index: 999999999999999; overflow: hidden; white-space: nowrap; width: auto; margin: 0; padding: .15em; background: -webkit-linear-gradient(top, #FFF 0px, #B0B0B0 1px, #A7A7A7); color: #333; text-shadow: 0 1px 0 #D0D0D0; border: 1px solid #6A6A6A; border-left: none; border-top-right-radius: 3px; font: 12px "Lucida Grande"; opacity: 0; -webkit-transition: opacity 100ms ease-out;');
   }
   
 }
